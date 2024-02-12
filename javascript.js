2c066f2214938ab65b0acab6bf3ee905ac36fd50
Anderson Soares <andersonsoares.jn@gmail.com>
2024-02-12 11:05:39 -0300

    fix: Resize the "squares" auto in container. Now is possible to change the size of grade, like 16x16 or 32x32, without change the size of container.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,12 @@
 function game(){
     const container = document.querySelector(".container");
 
-    for(let line = 1; line <= 16; line++){
+    for(let line = 1; line <= 100; line++){
         const containerLine = document.createElement("div");
+        containerLine.classList.add("containerLine");
         container.append(containerLine);
 
-        for(let item = 1; item <= 16; item++){
+        for(let item = 1; item <= 100; item++){
             const lineItem = document.createElement("div");
             lineItem.classList.add("lineItem");
             containerLine.append(lineItem);
@@ -16,7 +17,7 @@ function game(){
     containerItens.forEach(lineItem => lineItem.addEventListener("mouseover", () => {
         lineItem.style.backgroundColor = "black";
     }))
-    
+
 }
 
 game();
