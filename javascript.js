9d261bb558e7835f5628b5463c3cbbd25390632d
Anderson Soares <andersonsoares.jn@gmail.com>
2024-02-12 11:49:47 -0300

    feat: Add a button to resize the grade.

--- javascript.js
+++ javascript.js
@@ -1,13 +1,20 @@
 function game(){
     const container = document.querySelector(".container");
+    let containerLine;
+    let lineItem;
 
-    for(let line = 1; line <= 100; line++){
-        const containerLine = document.createElement("div");
+    let gradeSize = 16;
+    
+    let grade = document.createElement("div")
+    grade.classList.add("grade");
+    container.appendChild(grade);
+    for(let line = 1; line <= gradeSize; line++){
+        containerLine = document.createElement("div");
         containerLine.classList.add("containerLine");
-        container.append(containerLine);
+        grade.append(containerLine);
 
-        for(let item = 1; item <= 100; item++){
-            const lineItem = document.createElement("div");
+        for(let item = 1; item <= gradeSize; item++){
+            lineItem = document.createElement("div");
             lineItem.classList.add("lineItem");
             containerLine.append(lineItem);
         }
@@ -18,6 +25,41 @@ function game(){
         lineItem.style.backgroundColor = "black";
     }))
 
+    const resizeButton = document.querySelector(".resizeButton");
+    resizeButton.addEventListener("click" , () => {
+
+        gradeSize = window.prompt("Type the new grade: (max:100)")
+
+        let validAnswer = false;
+        while(!validAnswer){
+            if(gradeSize <= 100 && gradeSize >= 16){
+                validAnswer = true;
+            } else {
+                gradeSize = window.prompt("Please, type a valid answer: (min:16 max:100)")
+            }
+        }
+
+        container.removeChild(grade);
+
+        grade = document.createElement("div")
+        grade.classList.add("grade");
+        container.appendChild(grade);
+        for(let line = 1; line <= gradeSize; line++){
+            containerLine = document.createElement("div");
+            containerLine.classList.add("containerLine");
+            grade.append(containerLine);
+    
+            for(let item = 1; item <= gradeSize; item++){
+                lineItem = document.createElement("div");
+                lineItem.classList.add("lineItem");
+                containerLine.append(lineItem);
+            }
+        } 
+        const containerItens = document.querySelectorAll(".lineItem");
+        containerItens.forEach(lineItem => lineItem.addEventListener("mouseover", () => {
+        lineItem.style.backgroundColor = "black";
+    }))
+    })
 }
 
 game();
