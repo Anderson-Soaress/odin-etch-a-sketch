3ff7a5557928292b79243b137336c8b0a04cac6c
Anderson Soares <andersonsoares.jn@gmail.com>
2024-02-12 17:22:38 -0300

    feat: Add new buttons and litlle css change.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 function game(){
-    const container = document.querySelector(".container");
+    const container = document.querySelector(".flexContainer");
     let containerLine;
     let lineItem;
 
@@ -20,18 +20,45 @@ function game(){
         }
     } 
 
+    let color = "black";
+    
+    let inputColor = document.querySelector("#inputColor");
+    inputColor.addEventListener("change", () => {
+        color = inputColor.value;
+    })
+
+    const eraserButton = document.querySelector(".eraserButton");
+    eraserButton.addEventListener("click", () => {
+        color = "white";
+    })
+
     const containerItens = document.querySelectorAll(".lineItem");
     containerItens.forEach(lineItem => lineItem.addEventListener("mouseover", () => {
-        lineItem.style.backgroundColor = "black";
+        lineItem.style.backgroundColor = color;
     }))
 
     const clearButton = document.querySelector(".clearButton");
     clearButton.addEventListener("click", () => {
         const containerItens = document.querySelectorAll(".lineItem");
-        containerItens.forEach(lineItem => lineItem.style.backgroundColor = "white")
+        containerItens.forEach(lineItem => lineItem.style.backgroundColor = "white");
 
     })
 
+    let lineOn = true;
+
+    const toggleLineButton = document.querySelector(".toggleLineButton");
+    toggleLineButton.addEventListener("click", () => {
+        if(lineOn){
+            const containerItens = document.querySelectorAll(".lineItem");
+            containerItens.forEach(lineItem => lineItem.style.border = "none");
+            lineOn = false;
+        } else {
+            const containerItens = document.querySelectorAll(".lineItem");
+            containerItens.forEach(lineItem => lineItem.style.border = "1px solid rgb(150, 150, 150)");
+            lineOn = true;
+        }
+    })
+    
     const resizeButton = document.querySelector(".resizeButton");
     resizeButton.addEventListener("click" , () => {
 
